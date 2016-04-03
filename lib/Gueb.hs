6e2406674dfd4519bb17de98b4f65f11ce0780c6
ddc <diaz_carrete@yahoo.com>
2016-04-04 00:14:21 +0200

    ...

--- lib/Gueb.hs
+++ lib/Gueb.hs
@@ -5,8 +5,10 @@ module Gueb (
 
 import Control.Lens
 import Data.Monoid
+import Data.Functor
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TVar
+import Control.Concurrent.Async
 import Control.Monad.Trans.Except
 import Control.Monad.IO.Class
 
@@ -25,23 +27,24 @@ makeHandlers plan = do
     pure (makeHandlersFromRef tvar)
     
 -- http://haskell-servant.readthedocs.org/en/tutorial/tutorial/Server.html
-makeHandlersFromRef :: TVar (Jobs ()) -> Server JobsAPI 
+makeHandlersFromRef :: TVar (Jobs (Async ())) -> Server JobsAPI 
 makeHandlersFromRef ref =   
          (do 
-             jobs <- readState 
+             jobs <- readState_ 
              pure (Page jobs))
     :<|> (\jobid -> do
-             jobs <- readState 
+             jobs <- readState_ 
              undefined)
     :<|> (\jobid -> do
-             jobs <- readState 
+             jobs <- readState_ 
              case jobs ^. to getJobs . at jobid of
                  Nothing  -> throwE err404
                  Just jobex -> return (Page jobex))
     :<|> (\jobid execid -> do
-             jobs <- readState
+             jobs <- readState_
              case jobs ^.. to getJobs . at jobid . folded . executions . at execid . folded of
                  []  -> throwE err404
                  exe : _ -> return (Page exe))
     where
     readState = liftIO (atomically (readTVar ref))
+    readState_ = void <$> readState
