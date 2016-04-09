9906f2c909e859437e4b890f2314ade5854647e5
ddc <diaz_carrete@yahoo.com>
2016-04-10 00:46:36 +0200

    ugly,but complete

--- lib/Gueb.hs
+++ lib/Gueb.hs
@@ -1,21 +1,32 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# language DataKinds #-}
+{-# language TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Gueb (
         noJobs
     ,   makeHandlers
     ) where
 
 import Control.Lens
+import Data.Text
 import Data.Monoid
 import Data.Functor
+import Control.Monad
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TMVar
+import Control.Concurrent.STM.TChan
 import Control.Concurrent.Async
 import Control.Monad.Trans.Except
 import Control.Monad.IO.Class
 
 import Servant
+import Servant.HTML.Lucid (HTML)
 
 import Gueb.Types
 import Gueb.Types.API
+
+import System.Process.Streaming
 
 noJobs :: Jobs ()
 noJobs = Jobs mempty
@@ -30,21 +41,42 @@ makeHandlers plan = do
 makeHandlersFromRef :: TMVar (Jobs (Async ())) -> Server JobsAPI 
 makeHandlersFromRef ref =   
          (do 
-             jobs <- readState_ 
-             pure (Page jobs))
+             root <- readState_ 
+             pure (Page root))
     :<|> (\jobid -> do
-             jobs <- readState_ 
-             undefined)
+             root <- takeState 
+             unless (not (has (jobs . folded . executions . folded . bloh . _Right) root)) (do
+                 putState root
+                 throwE err409) -- conflict
+             ex <- case root ^. jobs . at jobid of
+                 Nothing -> do putState root
+                               throwE err404 -- not found
+                 Just ex  -> return ex
+             let Executions {_nextExecutionId,executable} = ex
+                 Job {scriptPath} = executable
+                 nextExecutionIdInt = succ _nextExecutionId
+                 nextExecutionIdText = Data.Text.pack (show nextExecutionIdInt) 
+             a <- liftIO (async (execute (piped (proc scriptPath [])) (pure ())))
+             let newExecution = Execution {blah="",_bloh=Right a}
+                 ex' = ex & nextExecutionId .~ nextExecutionIdInt
+                          & executions . at nextExecutionIdText .~ Just newExecution
+                 root' = root & jobs . at jobid .~ Just ex'
+             putState root'
+             let linkProxy = Proxy :: (Proxy ("jobs" :> Capture "jobid" Text :> "executions" :> Capture "execid" Text :> Get '[JSON,HTML] (Page (Execution ()))))
+                 linkUri = show (safeLink (Proxy :: Proxy JobsAPI) linkProxy jobid nextExecutionIdText)
+             return (addHeader linkUri (Page (Created linkUri))))
     :<|> (\jobid -> do
-             jobs <- readState_ 
-             case jobs ^. to getJobs . at jobid of
-                 Nothing  -> throwE err404
+             root <- readState_ 
+             case root ^. jobs . at jobid of
+                 Nothing -> throwE err404
                  Just jobex -> return (Page jobex))
     :<|> (\jobid execid -> do
-             jobs <- readState_
-             case jobs ^.. to getJobs . at jobid . folded . executions . at execid . folded of
+             root <- readState_
+             case root ^.. jobs . at jobid . folded . executions . at execid . folded of
                  []  -> throwE err404
                  exe : _ -> return (Page exe))
     where
     readState = liftIO (atomically (readTMVar ref))
     readState_ = void <$> readState
+    takeState = liftIO (atomically (takeTMVar ref))
+    putState j = liftIO (atomically (putTMVar ref j))
--- lib/Gueb/Types/API.hs
+++ lib/Gueb/Types/API.hs
@@ -22,13 +22,16 @@ import Servant.HTML.Lucid
 
 -- http://haskell-servant.readthedocs.org/en/stable/tutorial/ApiType.html
 type JobsAPI = "jobs" :> Get '[JSON,HTML] (Page (Jobs ())) 
-          :<|> "jobs" :> Capture "jobid" Text :> PostCreated '[JSON,HTML] (Headers '[Header "Location" Text] (Page Created))
+          :<|> "jobs" :> Capture "jobid" Text :> PostCreated '[JSON,HTML] (Headers '[Header "Location" String] (Page Created))
           :<|> "jobs" :> Capture "jobid" Text :> Get '[JSON,HTML] (Page (Executions Job ()))
           :<|> "jobs" :> Capture "jobid" Text :> "executions" :> Capture "execid" Text :> Get '[JSON,HTML] (Page (Execution ()))
 
 newtype Page a = Page { getContent :: a } deriving (Show,Generic,ToJSON)
 
-newtype Jobs async = Jobs { getJobs :: Map Text (Executions Job async) } deriving (Show,Generic,ToJSON,Functor)
+newtype Jobs async = Jobs { _jobs :: Map Text (Executions Job async) } deriving (Show,Generic,ToJSON,Functor)
+
+jobs :: Lens' (Jobs async) (Map Text (Executions Job async)) 
+jobs = lens _jobs (\r v -> r { _jobs = v }) 
 
 instance ToHtml (Jobs ()) where
     toHtml _ = return ()
@@ -40,10 +43,13 @@ instance ToHtml (Page (Jobs ())) where
 
 data Executions script async = Executions 
     {
-        nextExecutionId :: Int
+        _nextExecutionId :: Int
     ,   _executions     :: Map Text (Execution async)
     ,   executable      :: script 
     } deriving (Show,Generic,ToJSON,Functor)
+
+nextExecutionId :: Lens' (Executions script async) Int
+nextExecutionId = lens _nextExecutionId (\r v -> r { _nextExecutionId = v }) 
 
 executions :: Lens' (Executions script async) (Map Text (Execution async))
 executions = lens _executions (\r v -> r { _executions = v }) 
@@ -59,8 +65,11 @@ instance ToHtml a => ToHtml (Page (Executions a ())) where
 data Execution a = Execution
     {
         blah :: Text
-    ,   bloh :: Either Text a
+    ,   _bloh :: Either Text a
     } deriving (Show,Generic,ToJSON,Functor)
+
+bloh :: Lens' (Execution async) (Either Text async)
+bloh = lens _bloh (\r v -> r { _bloh = v }) 
 
 instance ToHtml (Execution ()) where
     toHtml _ = return ()
