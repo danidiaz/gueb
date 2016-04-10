4b63e4f59f89af243c96b100657bea0633414796
ddc <diaz_carrete@yahoo.com>
2016-04-10 13:22:58 +0200

    comonads!

--- lib/Gueb.hs
+++ lib/Gueb.hs
@@ -12,6 +12,7 @@ module Gueb (
 import Data.Text
 import Data.Monoid
 import Data.Functor
+import Data.Bifunctor
 import Control.Lens
 import Control.Monad
 import Control.Comonad
@@ -41,30 +42,33 @@ makeHandlers plan = do
         idstream = Data.Text.pack . show <$> unfold (succ . runIdentity) (pure 0)
     tvar <- atomically (newTMVar (idstream,jobs))
     pure (makeHandlersFromRef tvar)
-    
+
+maybeE :: Monad m => e -> Maybe a -> ExceptT e m a   
+maybeE e = maybe (throwE e) pure 
+
 -- http://haskell-servant.readthedocs.org/en/tutorial/tutorial/Server.html
 makeHandlersFromRef :: TMVar (Coiter Text,Jobs (Async ())) -> Server JobsAPI 
 makeHandlersFromRef ref =   
          (query id)
     :<|> (\jobid        -> query (jobs . ix jobid))
     :<|> (\jobid execid -> query (jobs . ix jobid . executions . ix execid))
-    :<|> (\jobid -> command (\pristine@(runCoiter -> (currentId,nextId),root) -> do
+    :<|> (\jobid -> command (\(advance -> (counter,root)) -> do
              unless (not (has (jobs . folded . executions . folded . bloh . _Right) root)) 
                     (throwE err409) -- conflict, some job already running
              ex <- maybeE err404 -- not found
                           (root ^. jobs . at jobid)
              let Executions {executable} = ex
                  Job {scriptPath} = executable
+                 executionId = extract counter
              a <- liftIO (async (execute (piped (proc scriptPath [])) (pure ())))
              let newExecution = Execution {blah="started",_bloh=Right a}
-                 ex' = ex & executions . at currentId .~ Just newExecution
+                 ex' = ex & executions . at executionId .~ Just newExecution
                  root' = root & jobs . at jobid .~ Just ex'
-                 linkUri = show (safeLink (Proxy :: Proxy JobsAPI) (Proxy :: Proxy ExecutionEndpoint) jobid currentId)
-             return ((nextId,root'),addHeader linkUri (Page (Created linkUri)))))
+                 linkUri = show (safeLink (Proxy :: Proxy JobsAPI) (Proxy :: Proxy ExecutionEndpoint) jobid executionId)
+             return ((counter,root'),addHeader linkUri (Page (Created linkUri)))))
     where
     readState_ = void . extract <$> liftIO (atomically (readTMVar ref))
     putState   = \j -> liftIO (atomically (putTMVar ref j))
-    maybeE e = maybe (throwE e) pure 
     query somelens = do root <- readState_ 
                         Page <$> maybeE err404
                                         (root ^? somelens)
@@ -74,3 +78,4 @@ makeHandlersFromRef ref =
                                    return result)
                                (\e -> do putState s
                                          throwE e)
+    advance = first (runIdentity . unwrap)
