8b9f5e7d9c26ed1f9144e74453ac15df9908cc47
ddc <diaz_carrete@yahoo.com>
2016-04-11 09:38:13 +0200

    refactoring for clarity

--- lib/Gueb.hs
+++ lib/Gueb.hs
@@ -52,6 +52,15 @@ makeHandlers plan = do
 maybeE :: Monad m => e -> Maybe a -> ExceptT e m a   
 maybeE e = maybe (throwE e) pure 
 
+launch :: Job -> IO () -> ExceptT e IO (Execution (Async ())) 
+launch (Job {scriptPath}) after = do 
+    a <- liftIO (async (do
+            (do execute (piped (proc scriptPath [])) (pure ())
+                after
+                pure ()) 
+                `onException` after))
+    pure (Execution {blah="started",_bloh=Right a})
+
 -- http://haskell-servant.readthedocs.org/en/tutorial/tutorial/Server.html
 makeHandlersFromRef :: MVar (Unending Text,Jobs (Async ())) -> Server JobsAPI 
 makeHandlersFromRef ref =   
@@ -62,35 +71,25 @@ makeHandlersFromRef ref =
              unless (not (has (_2 . jobs . folded . executions . folded . bloh . _Right) root)) 
                     (throwE err409) -- conflict, some job already running
              let Traversal ixJob = Traversal (_2 . jobs . ix jobid)
-                 Traversal atExecution = Traversal (ixJob . executions . at executionId) -- to add
-                 linkUri = show (safeLink jobsAPI executionEndpoint jobid executionId)
              script <- maybeE err404 -- job not found
                               (root ^? ixJob . executable)
+             let Traversal atExecution = Traversal (ixJob . executions . at executionId) -- to add
              launched <- launch script
                                 (notifyJobFinished (atExecution . _Just . bloh))
-             return (root & atExecution .~ Just launched,
-                     addHeader linkUri (Page (Created linkUri)))))
+             let root' = root & atExecution .~ Just launched
+                 linkUri = show (safeLink jobsAPI executionEndpoint jobid executionId)
+             return (root',addHeader linkUri ((Created linkUri)))))
     where
-    readState_ = void . extract <$> liftIO (readMVar ref)
-    putState   = \j -> liftIO (putMVar ref j)
-    query somelens = do root <- readState_ 
+    query somelens = do root <- void . extract <$> liftIO (readMVar ref)
                         Page <$> maybeE err404
                                         (root ^? somelens)
-    command  = \f -> do s <- liftIO (takeMVar ref)
-                        catchE (do (newState,result) <- f s 
-                                   putState newState
-                                   return result)
-                               (\e -> do putState s
+    command  = \f -> do oldState <- liftIO (takeMVar ref)
+                        catchE (do (newState,result) <- f oldState
+                                   liftIO (do putMVar ref newState
+                                              return (Page <$> result)))
+                               (\e -> do liftIO (putMVar ref oldState)
                                          throwE e)
     advance = first extract . duplicate . first (runIdentity . unwrap)
-    launch :: Job -> IO () -> ExceptT e IO (Execution (Async ())) 
-    launch (Job {scriptPath}) after = do 
-        a <- liftIO (async (do
-                (do execute (piped (proc scriptPath [])) (pure ())
-                    after
-                    pure ()) 
-                    `onException` after))
-        pure (Execution {blah="started",_bloh=Right a})
     notifyJobFinished executionPointer = do
         _ <- withMVar ref (\s -> evaluate (s & executionPointer .~ Left "fff"))
         pure ()
