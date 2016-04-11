5e9707bda171e19ef9a137557eaf8767d3e9c2a6
ddc <diaz_carrete@yahoo.com>
2016-04-11 23:49:26 +0200

    Adding some ToHtml instances.

--- lib/Gueb/Types/API.hs
+++ lib/Gueb/Types/API.hs
@@ -4,10 +4,11 @@
 {-# language DeriveFunctor #-}
 {-# language DeriveGeneric #-}
 {-# language DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Gueb.Types.API where
 
-import Data.Text (Text)
+import Data.Text (Text,pack)
 import Data.Map.Strict
 import Control.Lens
 import qualified Data.Map.Strict as Map
@@ -29,7 +30,6 @@ type JobsAPI = "jobs" :> Get '[JSON,HTML] (Page (Jobs ()))
 type ExecutionEndpoint = 
                "jobs" :> Capture "jobid" Text :> "executions" :> Capture "execid" Text :> Get '[JSON,HTML] (Page (Execution ()))
 
-newtype Page a = Page { getContent :: a } deriving (Show,Generic,ToJSON)
 
 newtype Jobs async = Jobs { _jobs :: Map Text (Executions Job async) } deriving (Show,Generic,ToJSON,Functor)
 
@@ -38,11 +38,11 @@ jobs = lens _jobs (\r v -> r { _jobs = v })
 
 instance ToHtml (Jobs ()) where
     toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtmlRaw = toHtml
 
 instance ToHtml (Page (Jobs ())) where
     toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtmlRaw = toHtml
 
 data Executions script async = Executions 
     {
@@ -57,12 +57,13 @@ executable :: Lens' (Executions script async) script
 executable = lens _executable (\r v -> r { _executable = v }) 
 
 instance ToHtml a => ToHtml (Executions a ()) where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml _ = div_ $ do
+        undefined
+    toHtmlRaw = toHtml
 
 instance ToHtml a => ToHtml (Page (Executions a ())) where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml    = pageWithTitle "Executions"
+    toHtmlRaw = toHtml
 
 data Execution a = Execution
     {
@@ -74,12 +75,18 @@ bloh :: Lens' (Execution async) (Either Text async)
 bloh = lens _bloh (\r v -> r { _bloh = v }) 
 
 instance ToHtml (Execution ()) where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml c = div_ $ do
+        p_ $ do "Execution started at: "
+                toHtml (blah c)
+        case _bloh c of
+            Left start -> p_ $ do "Execution finished at: "
+                                  toHtml start
+            Right ()   -> p_ $ do "Execution still ongoing"
+    toHtmlRaw = toHtml
 
 instance ToHtml (Page (Execution ())) where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml    = pageWithTitle "Execution"
+    toHtmlRaw = toHtml
 
 data Job = Job 
     {
@@ -87,16 +94,30 @@ data Job = Job
     } deriving (Show,Generic,FromJSON,ToJSON)
 
 instance ToHtml Job where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml c = div_ $ p_ $ do
+        "Script path: "
+        toHtml (scriptPath c)
+    toHtmlRaw = toHtml
 
+instance ToHtml (Page Job) where
+    toHtml    = pageWithTitle "Job"
+    toHtmlRaw = toHtml
+    
 newtype Created = Created { getLink :: String } deriving (Show,Generic,ToJSON)
 
 instance ToHtml Created where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml (Created link) = div_ $ p_ $ do
+        "Resource created at: "
+        a_ [href_ (pack link)] (toHtml link)
+    toHtmlRaw = toHtml
 
 instance ToHtml (Page Created) where
-    toHtml _ = return ()
-    toHtmlRaw _ = return ()
+    toHtml    = pageWithTitle "Resource created"
+    toHtmlRaw = toHtml
 
+newtype Page a = Page { getContent :: a } deriving (Show,Generic,ToJSON)
+
+pageWithTitle :: (Monad m, ToHtml contents) => Text -> contents -> HtmlT m ()
+pageWithTitle title contents = html_ $ do
+    head_ (title_ (toHtml title))
+    body_ (toHtml contents)
